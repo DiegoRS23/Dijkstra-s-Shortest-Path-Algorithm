5583f56a033f3bba154200f711256b869a228816
DiegoRS23 <58001860+DiegoRS23@users.noreply.github.com>
2020-02-25 11:34:29 -0600

    Add files via upload

--- dijkstra's-shortest-path-algorithm.cpp
+++ dijkstra's-shortest-path-algorithm.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #define INF INT_MAX
 
-int G[23][3] = {
+int G[23][3] = { // Graph array
 	{1,8},
 	{0,22},
 	{3,7,22},
@@ -29,7 +29,7 @@ int G[23][3] = {
 	{1,2,21}
 };
 
-int W[23][3] = {
+int W[23][3] = { // Weight array
 	{4,4},
 	{4,4},
 	{8,10,8},
@@ -55,15 +55,16 @@ int W[23][3] = {
 	{4,8,14}
 };
 
-int Deg[23] = {2,2,3,2,1,2,3,3,3,3,3,3,3,3,3,2,2,3,2,2,2,1,3};
+int Deg[23] = {2,2,3,2,1,2,3,3,3,3,3,3,3,3,3,2,2,3,2,2,2,1,3}; // Degree array
 
-int d[23];
+int d[23]; // Distance array
 
 typedef pair<int, int> node;
 
 void ShortestPath(int [][3],char schar) {
 	int s = -1;
 	
+	// Character to Integer Conversion
 	if (schar == 'A') {
 		s = 0;
 	}else if (schar == 'B'){
@@ -114,27 +115,27 @@ void ShortestPath(int [][3],char schar) {
 		s = -1;
 	}
 	
-	if (s != -1){
-	priority_queue<node, vector<node>, greater<node> > Q;
-	for (int v = 0; v < 23; v++) {
+	if (s != -1){ // If s does not equal -1 (input is valid) do
+	priority_queue<node, vector<node>, greater<node> > Q; // Queue gives priority to the node witht he smallest distance from the source node
+	for (int v = 0; v < 23; v++) { // Initialize the distance of the nodes to infinity
 		d[v] = INF;
 	}
-	d[s] = 0;
-	Q.push(make_pair(s,d[s]));
+	d[s] = 0; // Initialize the distance of the source node to 0
+	Q.push(make_pair(s,d[s])); // Enqueue s and d[s]
 	while (!Q.empty()) {
-	    int u = Q.top().first;
-	    Q.pop();
-	    for (int a = 0; a < Deg[u]; a++) {
-	        int v = G[u][a];
-	        int weight = W[u][a];
-	        if (d[v] > d[u] + weight) {
-	            d[v] = d[u] + weight;
-	            Q.push(make_pair(v,d[v]));
+	    int u = Q.top().first; // Set u to the node with the distance closest to the source
+	    Q.pop(); // Dequeue that node
+	    for (int a = 0; a < Deg[u]; a++) { // For each node adjacent to u do
+	        int v = G[u][a]; // Adjacent node to u
+	        int weight = W[u][a]; // Weight of the edge between node u and node v
+	        if (d[v] > d[u] + weight) { // If the distance of node v is greater than the distance of node u + the weight of the edge between the nodes do
+	            d[v] = d[u] + weight; // Set the distance of node v to the distance of node u + the weight of the edge between the nodes
+	            Q.push(make_pair(v,d[v])); // Enqueue the node
 	        }
 	    }
 	}
 	char min;
-	if (d[0] > d[5] and d[5] > d[19]){
+	if (d[0] > d[5] and d[5] > d[19]){ // Compare the distance of the source node to the exits and sets the node with the least distance to min
 	    min = 'T';
 	}else if (d[19] > d[0] and d[0] > d[5]){
 	    min = 'F';
@@ -144,7 +145,7 @@ void ShortestPath(int [][3],char schar) {
 	cout << "The distance of the source vertex " << schar << " from the exits are: " << endl;
 	cout << "Exit A: " << d[0] << endl << "Exit F: " << d[5] << endl << "Exit T: " << d[19] << endl;
 	cout << "The closest exit from the source vertex is: " << min << endl << endl;
-	}else{
+	}else{ // If the input is invalid do
 		cout << "***SOURCE VERTEX " << schar <<" DOESN'T EXIST IN THE GIVEN NETWORK***" << endl << endl;
 	}
 }
@@ -152,9 +153,10 @@ void ShortestPath(int [][3],char schar) {
 int main() {
 	
 	ShortestPath(G,'A');
-	ShortestPath(G, 'Z');
+	ShortestPath(G, 'P');
 	ShortestPath(G, 'M');
 	ShortestPath(G, 'G');
+	ShortestPath(G, 'Z');
 	
 	return 0;
 }
